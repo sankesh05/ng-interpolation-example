1ba04e805b34636e6c2f2dd7ebed94c1fca06a45
sankesh poojari <sankesh@shipwaves.com>
2019-10-26 22:01:53 +0530

    Frist commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  interpolation: [`(`, `)`]
+})
+export class AppComponent {
+  title = 'Custom Interpolation Demo1';
+}
